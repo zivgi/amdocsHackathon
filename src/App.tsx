74780c10a34891bff4796ae28e4fa8ad3ab95d89
zivgi <Ziv.Gilad@amdocs.com>
2022-03-24 10:49:57 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ServerAPI from './services/serverApi';
+
+const token = "";
+
+function App() {
+  useEffect(() => {
+    //ServerAPI.getHistory();
+    ServerAPI.getPendingBrs();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
